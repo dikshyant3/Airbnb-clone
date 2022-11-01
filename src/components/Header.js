eb049bbdb1e32c66e680d0022af06b8695e6bddc
Dikshyant <dhunganadikshyant12@gmail.com>
2022-11-01 17:51:51 +0545

    Home page started

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      {" "}
+      <img
+        className="header__icon"
+        src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Header;
